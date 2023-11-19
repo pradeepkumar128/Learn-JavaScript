7c3ae18658578b34806d2ea2698cf68a93cda1ee
iampradeep1282@gmail.com <iampradeep1282@gmail.com>
2023-11-19 13:10:11 +0530

    create stopwatch

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -1,1 +1,71 @@
-console.log("Hello");
+let hr = 0;
+let min = 0;
+let sec = 0;
+let count = 0;
+ let timer = false;
+
+function start(){
+ timer = true;
+ stopWatch();
+}
+
+function stop(){
+     timer = false;
+}
+
+function reset(){
+     timer = false;
+     let hr = 0;
+     let min = 0;
+     let sec = 0;
+     let count = 0;
+     document.getElementById("hr").innerHTML = "00";
+     document.getElementById("min").innerHTML = "00";
+     document.getElementById("sec").innerHTML = "00";
+     document.getElementById("count").innerHTML = "00";
+}
+
+function stopWatch(){
+ if(timer == true){
+     count = count + 1;
+     if(count == 100){
+          sec = sec + 1;
+          count = 0;
+     }
+     if(sec == 60){
+        min = min + 1;
+        sec = 0;  
+     }
+     if(min == 60) {
+          hr = hr + 1;
+          min = 0;
+          sec = 0;
+     }
+
+     let hrStr = hr;
+     let minStr = min;
+     let secStr = sec;
+     let countStr = count;
+
+     if(hr < 10){
+          hrStr = "0" + hrStr;
+     }
+     if(min < 10){
+          minStr = "0" + minStr;
+     }
+     if(sec < 10){
+          secStr = "0" + secStr;
+     }
+     if(countStr < 10){
+          countStr = "0" + countStr;
+     }
+     if(count < 10){
+          countStr = "0" + countStr;
+     }
+     document.getElementById("hr").innerHTML = hrStr;
+     document.getElementById("min").innerHTML = minStr;
+     document.getElementById("sec").innerHTML = secStr;
+     document.getElementById("count").innerHTML = countStr;
+   setTimeout("stopWatch()",10);
+ }
+ }
